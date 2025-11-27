cbf89baeaff8dc033c232efa336179c0fde6ba69
Ghita <takouitghita@gmail.com>
2025-11-27 11:05:49 +0100

    tp17

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const convert = require('xml-js');
 const protobuf = require('protobufjs');
 
-// Charger la définition Protobuf depuis employee.proto
 const root = protobuf.loadSync('employee.proto');
 const EmployeeList = root.lookupType('Employees');
 
-// Construire la liste d'employés
 const employees = [];
 
 employees.push({
@@ -27,48 +25,64 @@ employees.push({
   salary: 23000
 });
 
-// Objet racine compatible avec message Employees
 let jsonObject = { employee: employees };
 
-// ---------- JSON ----------
-
-// Sérialisation en JSON
+//  JSON : encodage 
+console.time('JSON encode');
 let jsonData = JSON.stringify(jsonObject);
+console.timeEnd('JSON encode');
 
-// ---------- XML ----------
+// JSON : décodage 
+console.time('JSON decode');
+let jsonDecoded = JSON.parse(jsonData);
+console.timeEnd('JSON decode');
 
-// Options de conversion JSON -> XML
+// XML 
+//conversion JSON -> XML
 const options = {
   compact: true,
   ignoreComment: true,
   spaces: 0
 };
 
-// Conversion en XML, avec balise racine <root>
+//XML : encodage
+console.time('XML encode');
 let xmlData = "<root>\n" + convert.json2xml(jsonObject, options) + "\n</root>";
+console.timeEnd('XML encode');
 
-// ---------- Protobuf ----------
+//XML : décodage 
+console.time('XML decode');
+// Conversion XML -> JSON (texte) -> objet JS
+let xmlJson = convert.xml2json(xmlData, { compact: true });
+let xmlDecoded = JSON.parse(xmlJson);
+console.timeEnd('XML decode');
 
+// Protobuf 
 // Vérification de conformité avec le schéma Protobuf
 let errMsg = EmployeeList.verify(jsonObject);
 if (errMsg) {
   throw Error(errMsg);
 }
 
-// Création du message Protobuf
+// Protobuf : encodage
+console.time('Protobuf encode');
 let message = EmployeeList.create(jsonObject);
-
-// Encodage en binaire Protobuf
 let buffer = EmployeeList.encode(message).finish();
+console.timeEnd('Protobuf encode');
 
-// ---------- Écriture des fichiers ----------
+//Protobuf : décodage
+console.time('Protobuf decode');
+let decodedMessage = EmployeeList.decode(buffer);
+// Optionnel : conversion vers objet JS "classique"
+let protoDecoded = EmployeeList.toObject(decodedMessage);
+console.timeEnd('Protobuf decode');
 
+// Écriture des fichiers 
 fs.writeFileSync('data.json', jsonData);
 fs.writeFileSync('data.xml', xmlData);
 fs.writeFileSync('data.proto', buffer);
 
-// ---------- Mesure des tailles ----------
-
+//Mesure des tailles
 const jsonFileSize = fs.statSync('data.json').size;
 const xmlFileSize = fs.statSync('data.xml').size;
 const protoFileSize = fs.statSync('data.proto').size;
